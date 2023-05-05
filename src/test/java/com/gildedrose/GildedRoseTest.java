c4a67ba87f666c31e32f4f1646337b5b9dfde93b
Laoul <abdoulraouf28@gmail.com>
2023-05-05 17:53:46 +0100

    Test on sulfuras item sellIn and quality never changing.

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -54,4 +54,13 @@ class GildedRoseTest {
         assertEquals(50, app.items[0].quality);
     }
 
+    @Test
+    public void sulfuras_item_quality_and_SellIn_never_changes() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 10, 80) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(10, app.items[0].sellIn);
+        assertEquals(80, app.items[0].quality);
+    }
+
 }
