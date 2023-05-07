5849e4e702fd14132bb3282bebec5dbb3bc7f7ac
Laoul <abdoulraouf28@gmail.com>
2023-05-07 14:00:32 +0100

    Simplify quality and sellIn updating operation from updateItemSellIn method.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -22,23 +22,23 @@ class GildedRose {
     private void updateItemSellIn(Item item) {
         if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
         } else {
-            item.sellIn = item.sellIn - 1;
+            item.sellIn -= 1;
         }
 
         if (item.sellIn < 0) {
             if (item.name.equals("Aged Brie")) {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                    item.quality += 1;
                 }
             } else {
                 if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    item.quality = item.quality - item.quality;
+                    item.quality -= item.quality;
                 } else {
                     if (item.quality > 0) {
                         if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
                             return;
                         }
-                        item.quality = item.quality - 1;
+                        item.quality -= 1;
                     }
                 }
             }
